d2db849e0ac331da340569cb529ac14f5be247fd
Mario-94 <mags9415@gmail.com>
2021-08-19 16:40:34 -0500

    pagina de inicio con footer y con la primera parte del encabezado, falta el agregar la ubicacion de las sucursales

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import "./App.css";
-import  Example  from "./Components/NavBar";
+import Example from "./Components/NavBar";
 function App() {
   return (
     <div className="App">
       {/* header  */}
-      <header >
-      <div>
-      <Example/>  
-      </div>
-        
+      <header>
+        <div>
+          <Example />
+        </div>
       </header>
       {/* body */}
       <body>
-        <p>En esta parte ira toda la informacio de la empreza </p>
+        <figure class="text-start">
+          <blockquote class="blockquote">
+            <p>
+              Por que tu economia nos interesa, Abarrotera de Pátzcuaro desde
+              1957 cuidando tu economia{" "}
+            </p>
+          </blockquote>
+          <figcaption class="blockquote-footer">
+            Someone famous in <cite title="Source Title">Source Title</cite>
+          </figcaption>
+        </figure>
       </body>
 
       {/* footer */}
 
-      <footer></footer>
+      <footer>
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
