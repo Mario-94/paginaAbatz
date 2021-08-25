5c53924faa9d82ce34c03f490c300f976ff1e467
Mario-94 <mags9415@gmail.com>
2021-08-25 13:57:56 -0500

    Terminado el componente home, solo falta  agregar el mapa y dar mas estilo a la pagina

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,42 @@
-import React, { Component } from 'react'
-import "../App.css"
+import React, { Component } from "react";
+import "../App.css";
 
 export class Home extends Component {
-    render() {
-        
-        return (
-            <div>
-               <h3>aqui ira el mapa</h3>
-               <h1 className="homeOferta" >Ofertas del día</h1>
-               
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>aqui ira el mapa</h3>
+        <h1 className="homeOferta">Ofertas de la semana </h1>
+
+        <div className="row ">
+          <div className="column">
+            <h2>Articulo</h2>
+            <p>Imagen</p>
+            <p>Descripcion</p>
+            <p>precio</p>
+          </div>
+          <div className="column">
+            <h2>Articulo</h2>
+            <p>Imagen</p>
+            <p>Descripcion</p>
+            <p>precio</p>
+          </div>
+          <div className="column">
+            <h2>Articulo</h2>
+            <p>Imagen</p>
+            <p>Descripcion</p>
+            <p>precio</p>
+          </div>
+          <div className="column">
+            <h2>Articulo</h2>
+            <p>Imagen</p>
+            <p>Descripcion</p>
+            <p>precio</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
