a4f7c3281872d5aadd12dea927d3f65e898a6011
Mario-94 <mags9415@gmail.com>
2021-09-01 14:03:50 -0500

    aun no se soluciona los errores de home, pero ya se agregaron las listas de productos y el formulario de contacto

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -1,13 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export class Contacto extends Component {
-    render() {
-        return (
-            <div>
-                <p>aqui val el formulario de contacto asi como los numeros telefonicos</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      
+       <Form>
+      <FormGroup row>
+        <Label for="exampleEmail" sm={2}>Correo</Label>
+        <Col sm={10}>
+          <Input type="email" name="email" id="exampleEmail" placeholder="Ejemplo@correo.com" />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="examplePassword" sm={2}>Contraseña</Label>
+        <Col sm={10}>
+          <Input type="password" name="password" id="examplePassword" placeholder="Contraseña" />
+        </Col>
+      </FormGroup>
+     
+      <FormGroup row>
+        <Label for="exampleText" sm={2}>Descripcion</Label>
+        <Col sm={10}>
+          <Input type="textarea" name="text" id="exampleText" />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="exampleFile" sm={2}>Adjuntar archivo</Label>
+        <Col sm={10}>
+          <Input type="file" name="file" id="exampleFile" />
+          <FormText color="muted">
+            Adjunte algun archivo referente a su queja o sugerencia
+          </FormText>
+        </Col>
+      </FormGroup>
+    
+      <FormGroup check row>
+        <Col sm={{ size: 10, offset: 2 }}>
+          <Button>Enviar</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+      
+    );
+  }
 }
 
-export default Contacto
+export default Contacto;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Container, Media, Col, Row } from "reactstrap";
 import "../App.css";
 
 export class Home extends Component {
@@ -6,33 +7,102 @@ export class Home extends Component {
     return (
       <div>
         <h3>aqui ira el mapa</h3>
-        <h1 className="homeOferta">Ofertas de la semana </h1>
-
-        <div className="row ">
-          <div className="column">
-            <h2>Articulo</h2>
-            <p>Imagen</p>
-            <p>Descripcion</p>
-            <p>precio</p>
-          </div>
-          <div className="column">
-            <h2>Articulo</h2>
-            <p>Imagen</p>
-            <p>Descripcion</p>
-            <p>precio</p>
-          </div>
-          <div className="column">
-            <h2>Articulo</h2>
-            <p>Imagen</p>
-            <p>Descripcion</p>
-            <p>precio</p>
-          </div>
-          <div className="column">
-            <h2>Articulo</h2>
-            <p>Imagen</p>
-            <p>Descripcion</p>
-            <p>precio</p>
-          </div>
+        <h1>Ofertas de la semana</h1>
+        <div>
+          <Container>
+            <Row xs="1" sm="2" md="4">
+              <Col xs="6">
+                <Media>
+                  <Media body>
+                    <Media heading>Articulo</Media>
+                    <Media heading> Unidad</Media>
+                    <Media heading> Precio</Media>
+                  </Media>
+                  <Media right top href="#">
+                    <Media
+                      object
+                      src="https://cdn7.kiwilimon.com//brightcove/6926/6926.jpg/"
+                      width="160"
+                      height="160"
+                      alt="Generic placeholder image"
+                    />
+                  </Media>
+                </Media>
+              </Col>
+              <Col xs="6">
+                <Media>
+                  <Media body>
+                    <Media heading>Articulo</Media>
+                    <Media heading> Unidad</Media>
+                    <Media heading> Precio</Media>
+                  </Media>
+                  <Media right top href="#">
+                    <Media
+                      object
+                      src="https://cdn7.kiwilimon.com//brightcove/6926/6926.jpg/"
+                      width="160"
+                      height="160"
+                      alt="Generic placeholder image"
+                    />
+                  </Media>
+                </Media>
+              </Col>
+              <Col xs="6">
+                <Media>
+                  <Media body>
+                    <Media heading>Articulo</Media>
+                    <Media heading> Unidad</Media>
+                    <Media heading> Precio</Media>
+                  </Media>
+                  <Media right top href="#">
+                    <Media
+                      object
+                      src="https://cdn7.kiwilimon.com//brightcove/6926/6926.jpg/"
+                      width="160"
+                      height="160"
+                      alt="Generic placeholder image"
+                    />
+                  </Media>
+                </Media>
+              </Col>
+              <Col xs="6">
+                <Media>
+                  <Media body>
+                    <Media heading>Articulo</Media>
+                    <Media heading> Unidad</Media>
+                    <Media heading> Precio</Media>
+                  </Media>
+                  <Media right top href="#">
+                    <Media
+                      object
+                      src="https://cdn7.kiwilimon.com//brightcove/6926/6926.jpg/"
+                      width="160"
+                      height="160"
+                      alt="Generic placeholder image"
+                    />
+                  </Media>
+                </Media>
+              </Col>
+              <Col xs="6">
+                <Media>
+                  <Media body>
+                    <Media heading>Articulo</Media>
+                    <Media heading> Unidad</Media>
+                    <Media heading> Precio</Media>
+                  </Media>
+                  <Media right top href="#">
+                    <Media
+                      object
+                      src="https://cdn7.kiwilimon.com//brightcove/6926/6926.jpg/"
+                      width="160"
+                      height="160"
+                      alt="Generic placeholder image"
+                    />
+                  </Media>
+                </Media>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </div>
     );
