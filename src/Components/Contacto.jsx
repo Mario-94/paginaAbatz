21a9efd266c3aadfe0ca8606e50ec05e61500b3a
Mario-94 <mags9415@gmail.com>
2021-09-07 10:39:56 -0500

    Se arreglo el formulario de contacto y se opto por cambir el carrusel por unas card para el menu de las marcas que maneja la empresa

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -6,98 +6,74 @@ import {
   Button,
   Form,
   FormGroup,
-  Label,
   Input,
-
-
 } from "reactstrap";
 
 export class Contacto extends Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Col xs={4}>
-            <Row>
-              
-            <h2>Que podemos hacer por ti </h2>
-            <br />
-              
-           
-             <p>Comunicate a nuestros siguientes modulos de atencion</p>
-           
-             <p>Tel:4343422648 o 434345968451</p>
-             <p>Correo electronico: abarrotera_patzcuaro@hotmail.com
-abatzcxc@gmail.com</p>
-            </Row>
-          </Col>
-          <Col xs="auto ">
-            <Form>
-              <FormGroup>
-                <Row>
-                  <Col xs="2">
-                    <Label for="exampleEmail">Correo</Label>
-                  </Col>
-                  <Col xs="10">
-                    <Input
-                      type="email"
-                      name="email"
-                      id="exampleEmail"
-                      placeholder="ejemplo@gmail.com"
-                    />
-                  </Col>
-                </Row>
-              </FormGroup>
+        {/* en esta parte es el mensaje  */}
+        <h1>Dejanos tu mensaje </h1>
+        <hr width="100%" />
+         {/* aqui empieza el formulario */}
+        <Form>
+          <Row>
+            {/* Esta parte es el nombre y apellido */}
+            <Col xs="6">
+              <Input
+                type="text"
+                name="name"
+                id="Name"
+                placeholder="Nombre(s)"
+              />
+            </Col>
 
-              <FormGroup>
-                <Row>
-                  <Col xs="2">
-                    <Label for="examplePassword">Telefono</Label>
-                  </Col>
-                  <Col xs="10">
-                    <Input
-                      type="text"
-                      name="telefono"
-                      id="exampletelefono"
-                      placeholder="Telefono o celular"
-                    />
-                  </Col>
-                </Row>
-              </FormGroup>
-              <FormGroup>
-                <Row>
-                  <Col xs="2">
-                    <Label for="exampleText">Descripcion </Label>
-                  </Col>
-                  <Col xs="10">
-                    <Input type="textarea" name="text" id="Descripcion" placeholder="Describe tu problema, queja o suferencia" />
-                  </Col>
-                </Row>
-              </FormGroup>
-              <FormGroup>
-                <Row>
-                  <Col xs="2">
-                    <Label for="exampleFile">
-                      Adjunte el archivo de queja o sugerencia
-                    </Label>
-                  </Col>
-                  <Col xs="10">
-                    <Input type="file" name="file" id="exampleFile" />
-                  </Col>
-                </Row>
-              </FormGroup>
-              <Row>
-                <Col xs="10">
-                  <FormGroup check>
-                    <Col sm={{ size: 10, offset: 2 }}>
-                      <Button>Enviar</Button>
-                    </Col>
-                  </FormGroup>
-                </Col>
-              </Row>
-            </Form>
-          </Col>
-        </Row>
+            <Col xs="6">
+              <Input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="Apellido"
+              />
+              <br />
+            </Col>
+            {/* en esta parte es el corre y numero telefonico */}
+            <Col xs="6">
+              <Input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Email"
+              />
+            </Col>
+
+            <Col xs="6">
+              <Input
+                type="text"
+                name="phoneNumber"
+                id="phoneNumber"
+                placeholder="Telefono o celular"
+              />
+              <br />
+            </Col>
+            <Col>
+              <Input
+                type="textarea"
+                name="email"
+                id="exampleTextarea"
+                placeholder="Descripcion"
+              />
+              <br />
+            </Col>
+
+            <FormGroup check row>
+              <Col>
+                <Button>Enviar</Button>
+              </Col>
+            </FormGroup>
+          </Row>
+        </Form>
       </Container>
     );
   }
