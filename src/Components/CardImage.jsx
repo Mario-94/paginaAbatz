516bcecca8911d42e205a70c900e83fe6c3a2563
Mario <mags9415@gmail.com>
2022-01-27 14:17:15 -0600

    ya se hace la peticion de imagenes, se ajusto el precio que es por producto, y se procede a arreglar estilos y paginacion

--- src/Components/CardImage.jsx
+++ src/Components/CardImage.jsx
@@ -13,52 +13,55 @@ import {
 } from "reactstrap";
 import ModalOfertas from "./ModalOfertas";
 import axios from 'axios';
-import React from 'react'
-
+import React from 'react';
+import "../App.css";
 export default class CardImage extends React.Component {
 
   state = {
     datos: [],
-    bandera:false,
-    img: "https://picsum.photos/318/180"
+    bandera: false,
+    //img: "https://picsum.photos/318/180"
   }
-  _valorReal(precio,porcentaje){
-  
-    const resultado=precio-((precio*porcentaje)/100)
+  _valorReal(precio, porcentaje) {
+
+    const resultado = precio - ((precio * porcentaje) / 100)
     return resultado.toFixed(2);
 
   }
+
   componentDidMount() {
     axios.get('http://localhost:5000/ofertasAlmacen')
       .then(res => {
         const datos = res.data;
-      console.log((datos).length)
-        this.setState({ datos, bandera:true})
+        // console.log((datos).length)
+        this.setState({ datos, bandera: true })
 
       }).catch(e => {
         console.log("Revisar su conexion", e)
       })
   }
   render() {
+
     return (
+
       <div>
         <Container >
           <Row xs="3">
-            {this.state.bandera=== true
+            {this.state.bandera === true
               ?
               this.state.datos.map((dato, index) =>
                 <Col key={index}>
                   <Card >
                     <CardImg
+
                       alt="Card image cap"
-                      src={this.state.img}
-                      top
-                      width="100%"
+                      src={`https://abattz.com/img/productos/${dato.Articulo}.jpg`}
+                      className="imagenes"
                     />
                     <CardBody>
                       <CardTitle tag="h5"> {dato.Descripcion1}</CardTitle>
                       { /*con toFixed(2) controlo el punto decimal de dos carecteres*/}
-                      <CardTitle tag="h5"> ${this._valorReal(dato.Precio,dato.Porcentaje)}</CardTitle>
+                      <CardTitle tag="h5"> ${this._valorReal(dato.Precio, dato.Porcentaje)}</CardTitle>
                       <CardSubtitle className="mb-2 text-muted" tag="h6">
                         Precio por:{dato.Unidad}
 
@@ -66,16 +69,16 @@ export default class CardImage extends React.Component {
                       <CardText>
 
                       </CardText>
-                      <ModalOfertas descripcion={dato.Descripcion1} producto={'titulo'} img={this.state.img} />
+                      {/* <ModalOfertas descripcion={dato.Descripcion1} producto={'titulo'} img={this.state.img} /> */}
                     </CardBody>
                   </Card>
                 </Col>
               )
               :
               <div >
-              <Spinner color="danger" type="border">{console.log("Error 500")}</Spinner>
+                <Spinner color="danger" type="border">{console.log("Error 500")}</Spinner>
               </div>
-              }
+            }
           </Row>
         </Container>
       </div>
