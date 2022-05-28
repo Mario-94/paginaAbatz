c6fece33667f4cd6a918eefb0cbcba9af9fed2fb
Mario <mags9415@gmail.com>
2022-05-27 20:04:53 -0500

    Se termino la aplicacion, ya se subio al host, falta revision para retroalimentacion.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -19,68 +19,58 @@ function NavBar() {
   return (
     <div className="navBar-container">
       {/* navINdex funcionara como el navbar  */}
-      {/* este lo tuilizare como si fuera el menu de hamburguesa */}
-      <div className="nav-logo-abatz">
-        <img src={logo} alt="logo" title="Abatz" className="logo" />{" "}
-        <div className="tituloAbatz"> ABATZ</div>
-      </div>
-      <FontAwesomeIcon
-        className="hamburger"
-        icon={faBars}
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded);
-        }}
-      />
+
       <div className="navBar">
+        {/* este lo tuilizare como si fuera el menu de hamburguesa */}
+        <div className="nav-logo-abatz">
+          <img src={logo} alt="logo" title="Abatz" className="logo" />{" "}
+          <a>ABATZ</a>
+        </div>
+        <FontAwesomeIcon
+          className="hamburger"
+          icon={faBars}
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        />
         <div
           className={
             isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
           }
         >
           <div className="navBarItems">
-            <Link className="" onClick={handleClick} to="/">
+            <Link className="" to="/">
               {/* agregar un icono de font awesome */}
               <FontAwesomeIcon className="" icon={faStore} title="Abatz" />
               Inicio
             </Link>
-
-            {/* se comento la linea de abajo por qu por lo pronto aun no presentremos las lineas que maneja la abarrotera, por que hay que preguntar bien como seran las lineas */}
-            {/* <a>
-              <Link className=""' to="/productos">
-                <FontAwesomeIcon icon={faBasketShopping} title='Productos' />
-                <h4> Productos </h4>
-              </Link>
-            </a> */}
-
-            <Link className="" onClick={handleClick} to="/sucursales">
+          </div>
+          <div className="navBarItems">
+            <Link className="" to="/sucursales">
               <FontAwesomeIcon icon={faBuildingColumns} title="Sucursales" />
               Sucursales
             </Link>
-
-            <Link className="" onClick={handleClick} to="/rutas">
+          </div>
+          <div className="navBarItems">
+            <Link className="" to="/rutas">
               <FontAwesomeIcon icon={faRoute} title="Rutas" />
               RUTAS
             </Link>
-
-            <Link className="" onClick={handleClick} to="/nosotros">
+          </div>
+          <div className="navBarItems">
+            <Link className="" to="/nosotros">
               <FontAwesomeIcon
                 icon={faAddressCard}
                 title="Acerca de nosotros"
               />
               Nosotros
             </Link>
-
-            <Link className="" onClick={handleClick} to="/contacto">
+          </div>
+          <div className="navBarItems">
+            <Link className="" to="/contacto">
               <FontAwesomeIcon icon={faHeadset} title="Contacto" />
               Contacto
             </Link>
-
-            {/* Se comento la linea de abajo por que no solo se utilizara para comprobar nuevos componentes */}
-            {/* <Link  to="/pruebas">
-               Pruebas
-              </Link> */}
-
-            {/* </Collapse> */}
           </div>
         </div>
       </div>
